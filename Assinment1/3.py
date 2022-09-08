1424eedd5ffd082d670f45354b5dac2a6cbc34a8
Abhishek <abhishekudiya09@gmail.com>
2022-09-08 21:40:20 +0530

    assignment upload

---
+++ Assinment1/3.py
@@ -0,0 +1,3 @@
+a=22
+name="Abhishek"
+print(a,name)
---
+++ Assinment1/4.py
@@ -0,0 +1,13 @@
+from tkinter import E
+
+
+name="Abhishek"
+age=22
+qual="MCA"
+year=2
+exp=0
+print("NAME",name)
+print("age",age)
+print("qualifivcation",qual)
+print("year",year)
+print("exp",exp)
